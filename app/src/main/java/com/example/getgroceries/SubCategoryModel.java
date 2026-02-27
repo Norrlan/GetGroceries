04efdfa488e4c4af14ea3e9751fb9652c9094142
N1116021 <oluwanifise.obabire2022@my.ntu.ac.uk>
2026-02-27 12:40:17 +0000

    Cleaned up the images for the categories in the search screen

---
+++ app/src/main/java/com/example/getgroceries/SubCategoryModel.java
@@ -0,0 +1,14 @@
+package com.example.getgroceries;
+
+public class SubCategoryModel
+{
+    String name;
+    int imageId;
+
+    public SubCategoryModel(String name, int imageId)
+    {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+}
